eacb1d331dcbca97f522df985388754969546d6e
crosszay1 <Isaiahbfreundlich@Gmail.com>
2026-03-10 10:40:31 -0400

    Added some API functions

---
+++ ConversionAPI/api.py
@@ -0,0 +1,13 @@
+from PIL import Image
+class api():
+    def __init__(self, image_path):
+        self.image = Image.open(image_path)
+    def get_dimensions(self):
+        width, height = self.image.size
+        return width, height
+    def get_format(self):
+        image_format = self.image.format
+        return image_format
+    def get_pixel(self, x, y):
+        pixel_value = self.image.getpixel((x, y))
+        return pixel_value
